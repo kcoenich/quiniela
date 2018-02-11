608a62d7d50d4b1049c8a53be59a099aa359e5e0
Jose Ignacio Ponce H <kcoenich@gmail.com>
2018-02-11 14:33:35 -0600

    login servlet modifications

--- src/main/LoginServlet.java
+++ src/main/LoginServlet.java
@@ -1,9 +1,23 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
 @WebServlet(
         name = "LoginServlet",
         description = "Login Servlet",
-        urlPatterns = {"/Login"}
+        urlPatterns = {"/bin/login"}
 )
 public class LoginServlet extends HTTPServlet  {
+
+    @Override
+    protected void doGet( HttpServletRequest request, HttpServletResponse response) '
+                        throws ServletException, IOException {
+
+        String jsonObject = "{}";
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        out.print(jsonObject);
+        out.flush();
+    }
 
 }
